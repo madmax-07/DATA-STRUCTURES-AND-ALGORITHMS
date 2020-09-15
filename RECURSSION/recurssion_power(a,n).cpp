6a5eda1b467051b98ed587bb43218a5cbac5c366
Shivani Walecha <54550506+madmax-07@users.noreply.github.com>
2020-09-15 13:49:30 +0530

    Add files via upload

---
+++ RECURSSION/recurssion_power(a,n).cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int power(int a, int n)
+{
+	if (n == 1)
+		return a;
+	else
+		return a * power(a, n - 1);
+
+
+}
+
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int a, n;
+		cin >> a >> n;
+		cout << power(a, n) << endl;
+	}
+
+
+}
