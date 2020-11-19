85ea011a2a803fb1a36f3d24ea5953e548d1296f
Shivani Walecha <54550506+madmax-07@users.noreply.github.com>
2020-11-19 15:30:45 +0530

    Add files via upload

--- arrays/maximum circular sum.cpp
+++ arrays/maximum circular sum.cpp
@@ -1,15 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 int kadane(int start, int end, vector<int>v) {
-	int n = v.size();
-	int sum = 0, maxsum = 0, s = start, i = s % n;
-	while (i != end) {
+
+	int sum = 0, maxsum = 0;
+	for (int i = start; i < end; i++) {
 		sum += v[i];
 		if (sum < 0)
 			sum = 0;
 		maxsum = max(sum, maxsum);
-		s++;
-		i = s % n;
+
 	}
 	return maxsum;
 }
@@ -21,26 +20,35 @@ int main() {
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 #endif
-	int t, n;
+
+	int t, n, i;
 	cin >> t;
 
 	while (t--) {
 		cin >> n;
-
 		vector<int>v(n);
-		for ( int i = 0; i < n; i++)
+		for (i = 0; i < n; i++)
 			cin >> v[i];
-		//travesring the array
-		int maxsum = 0;
-		int  start = 0, end = n - 1, end_index = end % n;
-		while (start <= n - 1) {
-			int sum = kadane(start, end_index, v);
-			maxsum = max(sum, maxsum);
-			start++;
-			end++;
-			end_index = end % n;
+		int umwrap_sum = kadane(0, n, v);
+		//this considers the edge case if alll the given elemnts in array are -ve
+		//i.e a[]={-3,-9,-2,-1}-> max sum =-1;
+		if (umwrap_sum == 0) {
+			sort(v.begin(), v.end(), greater<int>());
+
+			return v[0];
 		}
-		cout << maxsum << endl;
+		else {
+			int total_sum = 0;
+			for (i = 0; i < n; i++)
+
+			{	total_sum += v[i];
+				v[i] = -1 * v[i];
+			}
+			int non_contributingsum = kadane(0, n, v);
+			total_sum = total_sum + non_contributingsum;
+			int max_circularsum = max(total_sum, umwrap_sum);
+			cout << max_circularsum << endl;
+		}
 	}
 
 	return 0;
