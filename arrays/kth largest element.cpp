566b6d327aa5dc97f9148f2fa8f96e2a1d0117e5
Shivani Walecha <54550506+madmax-07@users.noreply.github.com>
2021-07-14 20:06:12 +0530

    Update kth largest element.cpp

--- arrays/kth largest element.cpp
+++ arrays/kth largest element.cpp
@@ -1,5 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+/*METHOD 1: SORT THE ARRAY AND THEN INDEX THE K-1th ELEMENT T(N)=nlogn and space complexity is o(n)
+
+OPTIMISED APPROACH:
+IN THE FOLLOWING METHOD WE SOLVE HAVE USED A TWEAKED VERSION OF QUCIK SORT METHOD WHERE WE STOP WHEN THE PARTITION INDEX IS K-1TH ELEMNT
+t(n)<nlogn and Sapce complexity=O(1) 
+
+*/
+
 int partition(vector<int> &v, int low, int high, int k)
 {
 	int pivot = v[high], pi = low;
@@ -30,7 +38,7 @@ int partition(vector<int> &v, int low, int high, int k)
 int findKthLargest(vector<int> &v, int k)
 {
 	int n = v.size();
-	return partition(v, 0, n - 1, n - k); //note if it woulfd hav ebeen smallest we would have just passsed k-1 instaed of n-k
+	return partition(v, 0, n - 1, n - k); //note if it woulD hav ebeen smallest we would have just passsed k-1 instaed of n-k
 }
 
 int main()
