7bd66ea46abfd4188aac22976cb26e8ecd4ed768
Shivani Walecha <54550506+madmax-07@users.noreply.github.com>
2021-10-05 21:34:16 +0530

    Update Construct Binary Search Tree from Preorder Traversal.cpp

--- BINARY TREES/BST/Construct Binary Search Tree from Preorder Traversal.cpp
+++ BINARY TREES/BST/Construct Binary Search Tree from Preorder Traversal.cpp
@@ -1,3 +1,6 @@
+question link :https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
+
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -9,12 +12,16 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/*Approach: traverse the array and do a preorder traversal keeping in mind that in bst nodes to the left of bst are in range [0,root->val) and nodes to right of bst are in range [root->val+1, MAX].
+*/
+
 class Solution {
 public:
-    TreeNode*solve(vector<int>&v,int&i,int min,int max){
+    TreeNode*solve(vector<int>&v,int&i,int min,int max){ //passing i (which keeps track of the array index) with refrence .
+        
         TreeNode*root=new TreeNode();
-        if(i==v.size())
-            return root;
+        if(i==v.size()) //base condition when i reaches the end of the array 
+            return NULL;
      root->val=v[i++];
      if(i<v.size() and (v[i]>=min && v[i]<=root->val-1))
         root->left=solve(v,i,min,root->val-1);
