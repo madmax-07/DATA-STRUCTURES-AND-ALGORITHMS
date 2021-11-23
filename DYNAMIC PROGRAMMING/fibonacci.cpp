1e29ef48e698e31ee27146a29a0148dfe9fb28dd
Shivani Walecha <54550506+madmax-07@users.noreply.github.com>
2021-11-23 17:53:17 +0530

    Update fibonacci.cpp

--- DYNAMIC PROGRAMMING/fibonacci.cpp
+++ DYNAMIC PROGRAMMING/fibonacci.cpp
@@ -9,17 +9,21 @@ int fib(n){
 time complexity:O(2^n)
 space complexity :O(n)->stack space
 */
+//DP solution 
+//TOP DOWN APPROACH - RECURSION + MEMOIZATION 
 class Solution {
 public:
     int fibo(int n ,int*dp){
         //base case
         if(n==0 ||n==1)
             return n;
-        //look up
+        //look up-Check if the ans already exist 
         if(dp[n]!=0)
             return dp[n];
         //recur case
-    return dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
+    int ans=fibo(n-1,dp)+fibo(n-2,dp);
+    dp[n]=ans; //save the ans for future use
+    return dp[n]
     }
     int fib(int n) {
         int dp[31]={0};
@@ -29,3 +33,14 @@ public:
  //time complexity:O(n)
  //space cpmplexiy :O(n)
 };
+//BOTTOM-UP APPROACH- This is basically known as Dp approach 
+int fib(int n int*dp)
+{
+  dp[0]=0;
+  dp[1]=1;
+for(int i=2;i<n;i++)
+{ dp[i]=dp[i-1]+dp[i-2];
+}
+return dp[n];
+}
+
