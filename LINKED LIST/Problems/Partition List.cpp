1cc28b81dd64278a582c8d174deead43ceb3bca5
Shivani Walecha <54550506+madmax-07@users.noreply.github.com>
2022-01-09 14:09:26 +0530

    leetcode 86

---
+++ LINKED LIST/Problems/Partition List.cpp
@@ -0,0 +1,103 @@
+/*
+Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,4,3,2,5,2], x = 3
+Output: [1,2,2,4,3,5]
+Example 2:
+
+Input: head = [2,1], x = 2
+Output: [1,2]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 200].
+-100 <= Node.val <= 100
+-200 <= x <= 200
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode*dummless=new ListNode(0);
+        ListNode*less=dummless;
+        ListNode*dumgrt=new ListNode(0);
+        ListNode* grtr=dumgrt;
+        ListNode*p=head;
+        while(p!=NULL){
+            if(p->val<x){
+                less->next=p;
+                less=less->next;
+            }
+            else {
+                grtr->next=p;
+                grtr=grtr->next;
+            }
+            p=p->next;
+        }
+        grtr->next=NULL;
+        less->next=dumgrt->next;
+        delete dumgrt;
+        head=dummless->next;
+        delete dummless; 
+        return head;
+        
+        
+        
+    }
+};/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode*dummless=new ListNode(0);
+        ListNode*less=dummless;
+        ListNode*dumgrt=new ListNode(0);
+        ListNode* grtr=dumgrt;
+        ListNode*p=head;
+        while(p!=NULL){
+            if(p->val<x){
+                less->next=p;
+                less=less->next;
+            }
+            else {
+                grtr->next=p;
+                grtr=grtr->next;
+            }
+            p=p->next;
+        }
+        grtr->next=NULL;
+        less->next=dumgrt->next;
+        delete dumgrt;
+        head=dummless->next;
+        delete dummless; 
+        return head;
+        
+        
+        
+    }
+};
